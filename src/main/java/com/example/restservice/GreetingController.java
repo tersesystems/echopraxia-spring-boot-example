66986e3ca4081223c3f9cc998c89ae1db136e047
Will Sargent <will.sargent@gmail.com>
2022-01-17 09:37:48 -0800

    Add log4j implementation

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -4,6 +4,7 @@ import com.tersesystems.echopraxia.Condition;
 import com.tersesystems.echopraxia.Logger;
 import com.tersesystems.echopraxia.LoggerFactory;
 import com.tersesystems.echopraxia.scripting.ScriptCondition;
+import com.tersesystems.echopraxia.scripting.ScriptWatchService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,19 +33,28 @@ public class GreetingController {
                 return fb.requestFields(request);
               });
 
-  private final Condition debugCondition = ScriptCondition.create(false,
-            Paths.get("condition.tf"),
-            e -> logger.error("Script failed!", e));
+  // This should generally be global to the application, as it creates a watcher thread internally
+  private final Path scriptDirectory = Paths.get("scripts").toAbsolutePath();
 
+  private final ScriptWatchService scriptWatchService = new ScriptWatchService(scriptDirectory);
+
+  // Creates a condition from a script and re-evaluates it whenever the script changes
+  private final Condition debugCondition =
+      ScriptCondition.create(
+          scriptWatchService.watchScript(
+              scriptDirectory.resolve("condition.tf"), e -> logger.error(e.getMessage(), e)));
+
+  // Creates a debug logger that will filter out any requests that doesn't meet the condition.
   private final Logger<HttpRequestFieldBuilder> debugLogger = logger.withCondition(debugCondition);
 
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     logger.info("Greetings {}", fb -> fb.onlyString("greeting_name", name));
 
-    debugLogger.debug("This message only shows up when request_remote_addr is 127.0.0.1 and level>=DEBUG");
+    // the logger must be set to DEBUG level and also meet the condition.
+    debugLogger.debug(
+        "This message only shows up when request_remote_addr is 127.0.0.1 and level>=DEBUG");
 
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
-
 }
